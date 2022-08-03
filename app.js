84d9b54d2060068e5b625343f811af17f7431f95
muratcanbastug <muratcanbastug1@gmail.com>
2022-08-04 00:34:43 +0300

    assignment_8

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require("express");
+const express = require('express');
+require('ejs');
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 app.listen(PORT, () => {
